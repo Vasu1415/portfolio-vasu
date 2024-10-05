ea004dc0fd08b63e90a4f327a079d547d14f825a
VasuMittalNt <vmittal1@terpmail.umd.edu>
2024-10-05 19:30:30 -0400

    project update

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -18,7 +18,7 @@ const projects = [
     description: 'A game where players guess which video game has more downloads. Choose your level of difficulty with caution!',
     githubLink: 'https://github.com/tomurdi/higherorlower',
     skills: ['Python', 'Flask', 'Tailwind CSS'],
-    liveDemo: 'https://higherorlowergame-vasu-mittals-projects-656b9b20.vercel.app/'
+    liveDemo: 'https://higherorlowergame-git-main-vasu-mittals-projects-656b9b20.vercel.app/'
   },
   {
     name: 'Kitchen Craft',
