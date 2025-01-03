f24793fc30bf20ed5151bdd368c0612e8bc1d7c9
Vasu Mittal <vmittal1@terpmail.umd.edu>
2025-01-02 20:33:51 -0500

    Matomo Integration 2

--- app/layout.js
+++ app/layout.js
@@ -12,14 +12,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   React.useEffect(() => {
-    var _mtm = (window._mtm = window._mtm || []);
-    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.async = true;
-    g.src = "https://cdn.matomo.cloud/portfoliovasumittalvercelapp.matomo.cloud/container_SUFKW7Gx.js";
-    s.parentNode.insertBefore(g, s);
+    var _paq = (window._paq = window._paq || []);
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+    _paq.push(["setCookieDomain", "*.portfolio-vasu-mittal.vercel.app"]);
+    _paq.push(["setDomains", ["*.portfolio-vasu-mittal.vercel.app"]]);
+    _paq.push(["trackPageView"]);
+    _paq.push(["enableLinkTracking"]);
+    (function () {
+      var u = "https://portfoliovasumittalvercelapp.matomo.cloud/";
+      _paq.push(["setTrackerUrl", u + "matomo.php"]);
+      _paq.push(["setSiteId", "1"]);
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src = "https://cdn.matomo.cloud/portfoliovasumittalvercelapp.matomo.cloud/matomo.js";
+      s.parentNode.insertBefore(g, s);
+    })();
   }, []);
 
   return (
